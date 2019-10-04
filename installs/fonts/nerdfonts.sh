d767590c56c75c5be4a255a2ef23af21a3ef5ffd
Gilbert F. Sewovoe-Ekoue <gilberts55@hotmail.com>
2019-10-04 16:51:18 -0400

    Add more fonts

--- installs/fonts/nerdfonts.sh
+++ installs/fonts/nerdfonts.sh
@@ -3,8 +3,16 @@
 echo -e "\nInstalling nerd font..."
 
 formulas=(
-  font-firacode-nerd-font
-  font-sourcecodepro-nerd-font-mono
+    font-dejavusansmono-nerd-font-mono
+    font-dejavusansmono-nerd-font
+    font-firacode-nerd-font
+    font-monoid-nerd-font
+    font-hack-nerd-font
+    font-hasklig-nerd-font
+    font-inconsolatago-nerd-font
+    font-iosevka-nerd-font
+    font-noto-nerd-font
+    font-sourcecodepro-nerd-font-mono
 )
 
 for formula in "${formulas[@]}"; do
